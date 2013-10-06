64925d9aeeb3fc370017c4f2b5f77297116598e0
herlad matias olivares zarsoza <heraldmatias.oz@gmail.com>
2013-10-05 21:27:53 -0500

    config

---
+++ django-sunat/project/development.py
@@ -0,0 +1,19 @@
+from project.settings import *
+DEBUG=True
+TEMPLATE_DEBUG=DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '../sunat.db' ,                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+import sys
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
