ac87b6c5b39262f1f4786a25ef1ec969d31f388b
herlad matias olivares zarsoza <heraldmatias.oz@gmail.com>
2013-10-06 16:10:41 -0500

    initial commit

--- symfony2-sunat/app/AppKernel.php
+++ symfony2-sunat/app/AppKernel.php
@@ -17,9 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new Upc\Cards\Bundle\SecurityBackendBundle\SecurityBackendBundle(),
-            new Upc\Cards\Bundle\SecurityFrontendBundle\SecurityFrontendBundle(),
-            new Upc\Cards\Bundle\CardsBundle\CardsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
